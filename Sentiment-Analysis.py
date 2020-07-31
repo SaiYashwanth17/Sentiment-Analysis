c50b89577e9577dbdd1f077460caaa28d132e136
SaiYashwanth17 <saiyashwanth1791999@gmail.com>
2020-07-31 18:50:53 +0530

    data cleaning updated

--- Sentiment-Analysis.py
+++ Sentiment-Analysis.py
@@ -1,22 +1,38 @@
 import re
 import tweepy
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import string
 from tweepy import OAuthHandler
 from textblob import TextBlob
+
+
 class TwitterClient(object):
+
     def __init__(self):
         # keys and tokens from the Twitter Dev Console
-        consumer_key = 'wO0bZkczfu0wcWbEUCJqRPMzD'
-        consumer_secret = 'jHjpRuGAs634AGCnGLCA8034J4HWZ0q0Q2MFjseJdRu1iUw0IW'
-        access_token = '1286628360337031168-XoLILZl5YPrBtsBkgOj7YojWNxpr7I'
-        access_token_secret = '0QPe6RLK8Q2mvgjovvSwUqIYsd7aWgEEP4jUxtxXXpDEU'
+        consumer_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxx'
+        consumer_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxx'
+        access_token = 'xxxxxxxxxxxxxxxxxxxxxxxxxx'
+        access_token_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxx'
         try:
             self.auth = OAuthHandler(consumer_key, consumer_secret)
             self.auth.set_access_token(access_token, access_token_secret)
             self.api = tweepy.API(self.auth)
         except:
             print("Authentication Failed")
+
     def clean_text(self, text):
-        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])| (\w+:\ / \ / \S+)", " ", text).split())
+        tokens = word_tokenize(text)
+        tokens = [w.lower() for w in tokens]
+        table = str.maketrans('', '', string.punctuation)
+        stripped = [w.translate(table) for w in tokens]
+        words = [word for word in stripped if word.isalpha()]
+        stop_words = set(stopwords.words('english'))
+        words = [w for w in words if not w in stop_words]
+        txt = ' '.join([word for word in words])
+        return txt
+
     def tweet_sentiment(self, tweet):
         analysis = TextBlob(self.clean_text(tweet))
         if analysis.sentiment.polarity > 0:
@@ -25,6 +41,7 @@ class TwitterClient(object):
             return 'neutral'
         else:
             return 'negative'
+
     def get_tweets(self, query, count = 10):
         tweets = []
         try:
@@ -39,21 +56,20 @@ class TwitterClient(object):
                 else:
                     tweets.append(parsed_tweet)
             return tweets
+
         except tweepy.TweepError as e:
             print("Error : " + str(e))
 
 
 def main():
-    # creating object of TwitterClient Class
     api = TwitterClient()
-    # calling function to get tweets
-    tweets = api.get_tweets(query='Donald Trump', count=200)
+    tweets = api.get_tweets(query='bill gates', count=200)
 
-    # picking positive tweets from tweets
+    # picking positive tweets
     ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
     # percentage of positive tweets
     print("Positive tweets percentage: {} %".format(100 * len(ptweets) / len(tweets)))
-    # picking negative tweets from tweets
+    # picking negative tweets
     ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
     # percentage of negative tweets
     print("Negative tweets percentage: {} %".format(100 * len(ntweets) / len(tweets)))
@@ -61,15 +77,15 @@ def main():
     print("Neutral tweets percentage: {} % \
           ".format(100 * (len(tweets) - (len(ntweets) + len(ptweets))) / len(tweets)))
 
-    # printing first 5 positive tweets
-    # print("\n\nPositive tweets:")
-    # for tweet in ptweets[:10]:
-    #     print(tweet['text'])
+    ## printing first 5 positive tweets
+    print("\n\nPositive tweets:")
+    for tweet in ptweets[:5]:
+        print(tweet['text'])
 
-    # # printing first 5 negative tweets
-    # print("\n\nNegative tweets:")
-    # for tweet in ntweets[:10]:
-    #     print(tweet['text'])
+    ## printing first 5 negative tweets
+    print("\n\nNegative tweets:")
+    for tweet in ntweets[:5]:
+        print(tweet['text'])
 
 if __name__ == "__main__":
     # calling main function
